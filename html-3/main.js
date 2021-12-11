0a512528eb830668ee62fcc7fd769d4756cdacfe
thien1720 <thien29032020@gmail.com>
2021-12-11 13:45:31 +0700

    html-3

---
+++ html-3/main.js
@@ -0,0 +1,12 @@
+//for 
+let myArray =[
+    [1,2],
+    [2,3],
+    [3,4]
+];
+
+for(let i = 0 ; i< myArray.length; i++){
+    for(let j =0; j <  myArray[i].length;j++){
+        console.log(myArray[i][j])
+    }
+}
